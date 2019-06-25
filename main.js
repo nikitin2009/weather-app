268bf887052e43f6a67bf427ea9add81cfe0e45d
Alex <nikitin2009@gmail.com>
2019-06-25 11:28:52 +0300

    Refactor code

--- main.js
+++ main.js
@@ -4,12 +4,6 @@ $(document).ready(function() {
       celsius,
       fahrenheit;
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getWeather);
-  } else {
-    $('.name').innerHTML = "Geolocation is not supported by this browser.";
-  }
-
   function getWeather(position) {
     
     var latitude = position.coords.latitude,
@@ -22,27 +16,52 @@ $(document).ready(function() {
         lon: longitude
       }
     })
-    .done(function(data) {
-      $('.name').text(data.name + ', ' + data.sys.country);
-      $('.icon').html('<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">');
-      $('.temp').text(Math.round(data.main.temp));
-      
-      celsius = Math.round(data.main.temp);
-      fahrenheit = Math.round(celsius * 9 / 5 + 32);
-      
-      $('.main').text(data.weather[0].main);
-      $('.description').text(data.weather[0].description);
-      $('.pressure').text(data.main.pressure);
-      $('.humidity').text(data.main.humidity);
-      $('.wind').text(data.wind.speed);
-      $('.arrow').css('transform', 'rotate(' + data.wind.deg + 'deg)');
-    })
+    .done(renderWeatherData)
     .error(function(err) {
       $('.name').text(err);
     });
-    
+  
   }
  
+  function renderWeatherData(data) {
+    $('.name').text(data.name + ', ' + data.sys.country);
+    $('.icon').html('<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">');
+    $('.temp').text(Math.round(data.main.temp));
+    
+    celsius = Math.round(data.main.temp);
+    fahrenheit = Math.round(celsius * 9 / 5 + 32);
+    
+    $('.main').text(data.weather[0].main);
+    $('.description').text(data.weather[0].description);
+    $('.pressure').text(data.main.pressure);
+    $('.humidity').text(data.main.humidity);
+    $('.wind').text(data.wind.speed);
+    $('.arrow').css('transform', 'rotate(' + data.wind.deg + 'deg)');
+  }
+  
+  function selectImg(images, temperature) {
+    if (temperature <= -20) return images['freeze'];
+    else if (-20 < temperature <= 10) return images['cold'];
+    else if (10 < temperature <= 20) return images['norm'];
+    else if (20 < temperature <= 30) return images['warm'];
+    else if  (temperature > 30) return images['hot'];
+  }
+
+  $('form#cityName').on('submit', e => {
+    e.preventDefault();
+
+  });
+
+  $('form#currentLocation').on('submit', e => {
+    e.preventDefault();
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(getWeather);
+    } else {
+      $('.name').innerHTML = "Geolocation is not supported by this browser.";
+    }
+  });
+
   $('.btn:first-child').on('click', function() {
     $('.temp').text(celsius);
     $('.btn:first-child').addClass('active').siblings().removeClass('active');
@@ -52,13 +71,5 @@ $(document).ready(function() {
     $('.temp').text(fahrenheit);
     $('.btn:last-child').addClass('active').siblings().removeClass('active');
   });
-  
-  function selectImg(images, temperature) {
-    if (temperature <= -20) return images['freeze'];
-    else if (-20 < temperature <= 10) return images['cold'];
-    else if (10 < temperature <= 20) return images['norm'];
-    else if (20 < temperature <= 30) return images['warm'];
-    else if  (temperature > 30) return images['hot'];
-  }
   
 });
